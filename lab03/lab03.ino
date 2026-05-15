9dfd01de90cfaf01adb0aa3285c28c06bbc17f83
jianxiong-gong <3385372867@qq.com>
2026-05-15 21:16:44 +0800

    lab03: 新建按键控制

---
+++ lab03/lab03.ino
@@ -0,0 +1,12 @@
+#define LED_PIN 13
+#define KEY_PIN 2
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(KEY_PIN, INPUT_PULLUP);
+}
+void loop() {
+  if (digitalRead(KEY_PIN) == LOW)
+    digitalWrite(LED_PIN, HIGH);
+  else
+    digitalWrite(LED_PIN, LOW);
+}
